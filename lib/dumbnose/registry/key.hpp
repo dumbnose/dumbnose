de65a4334fdd518c4b26dfca43dde77ecc09e79e
John Sheehan <jsheehan@live.com>
2019-12-19 16:53:24 -0800

    added some functionality for dealing with key values

--- lib/dumbnose/registry/key.hpp
+++ lib/dumbnose/registry/key.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <list>
+#include <map>
+
 
 namespace dumbnose { namespace registry {
 
@@ -8,9 +10,6 @@ class key
 {
 public:
 
-	typedef std::list<std::wstring> wstring_list;
-
-// constructors / destructor
 	key() : handle_(new handle_holder()) {}
 	~key() {}
 
@@ -18,6 +17,13 @@ public:
 	{
 		key hklm_key;
 		hklm_key.open(HKEY_LOCAL_MACHINE,L"",KEY_READ);
+		return hklm_key;
+	}
+
+	static key hkcu()
+	{
+		key hklm_key;
+		hklm_key.open(HKEY_CURRENT_USER, L"", KEY_READ);
 		return hklm_key;
 	}
 
@@ -37,15 +43,15 @@ public:
 		return subkey;
 	}
 
-	wstring_list enum_value_names()
+	std::list<std::wstring> enum_value_names()
 	{
-		wstring_list result;
+		std::list<std::wstring> result;
 		wchar_t name_buffer[1024];
 		DWORD type = 0;
 
 		for(int i=0 ;; ++i)
 		{
-			DWORD name_buffer_len = sizeof(name_buffer);
+			DWORD name_buffer_len = ARRAYSIZE(name_buffer);
 			LSTATUS status = RegEnumValue(handle_->handle(), i, name_buffer,&name_buffer_len,0,&type,0,0);
 			if(ERROR_NO_MORE_ITEMS==status) {
 				break;
@@ -59,28 +65,47 @@ public:
 		return result;
 	}
 
-	wstring_list enum_value_strings()
+	void clear_enum_values()
 	{
-		wstring_list result;
-		wchar_t name_buffer[1024];
-		byte value_buffer[2048];
+		auto values = enum_value_names();
+		for (auto value : values) {
+			RegDeleteValue(handle_->handle(), value.c_str());
+		}
+	}
+
+	std::map<std::wstring, std::wstring> enum_values_as_strings()
+	{
+		std::map<std::wstring, std::wstring> result;
+		wchar_t name_buffer[1024] = { 0 };
+		wchar_t value_buffer[4096] = { 0 };
 		DWORD type = 0;
 
 		for(int i=0 ;; ++i)
 		{
-			DWORD name_buffer_len = sizeof(name_buffer);
+			DWORD name_buffer_len = ARRAYSIZE(name_buffer);
 			DWORD value_buffer_len = sizeof(value_buffer);
-			LSTATUS status = RegEnumValue(handle_->handle(), i, name_buffer,&name_buffer_len,0,&type,value_buffer,&value_buffer_len);
+			LSTATUS status = RegEnumValue(handle_->handle(), i, name_buffer,&name_buffer_len,0,&type,reinterpret_cast<LPBYTE>(value_buffer),&value_buffer_len);
 			if(ERROR_NO_MORE_ITEMS==status) {
 				break;
 			} else if(ERROR_SUCCESS!=status) {
 				throw windows_exception("Could not enumerate values",status);
 			}
 
-			if((value_buffer_len>0) && (REG_SZ==type)) result.push_back(std::wstring(reinterpret_cast<wchar_t*>(value_buffer)/*,value_buffer_len/sizeof(wchar_t)*/));
+			if ((value_buffer_len > 0) && (REG_SZ == type)) result[name_buffer] = value_buffer;
 		}
 
 		return result;
+	}
+
+	void save_enum_values_as_strings(std::map<std::wstring, std::wstring> values)
+	{
+		// Clean up old values first
+		clear_enum_values();
+
+		for (auto value : values) {
+			LSTATUS status = RegSetValueEx(handle_->handle(), value.first.c_str(), 0, REG_SZ, reinterpret_cast<const BYTE*>(value.second.c_str()), value.second.length()*sizeof(wchar_t));
+			if (ERROR_SUCCESS != status) throw windows_exception("Could not enumerate values", status);
+		}
 	}
 
 protected:
@@ -115,7 +140,7 @@ protected:
 	}
 
 private:
-	shared_ptr<handle_holder> handle_;
+	std::shared_ptr<handle_holder> handle_;
 };
 
 
